1e0d199be89869d0833ff18b3de6eb8cd2fb3e25
Manhduc13 <manhduc130702@gmail.com>
2024-12-17 09:28:40 +0700

    Step 1: Create project

---
+++ src/main/java/com/nguyenducmanh/hotel_app_spring_api/HotelAppSpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.nguyenducmanh.hotel_app_spring_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelAppSpringApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelAppSpringApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nguyenducmanh/hotel_app_spring_api/HotelAppSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nguyenducmanh.hotel_app_spring_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelAppSpringApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
